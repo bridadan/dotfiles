6123e548f19b839e03a9ee8e9baffdf96c99b97f
Brian Daniels <brianddaniels@gmail.com>
2017-10-03 14:23:58 -0500

    Installing Vundle and YouCompleteMe

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@ apt-get install -y git vim tmux
 git config --global user.email "brianddaniels@gmail.com"
 git config --global user.name "Brian Daniels"
 git config --global core.editor "vim"
+
+cp .* ~/
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+~/.vim/bundle/YouCompleteMe/install.py --tern-completer
